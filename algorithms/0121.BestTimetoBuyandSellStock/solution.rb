b7938ea7926bc11cd6ccac621f1a778fff76f25d
Kohei Sugimoto <18735471+kohbis@users.noreply.github.com>
2020-01-27 16:27:59 +0900

    solved 0121.BestTimetoBuyandSellStock

---
+++ algorithms/0121.BestTimetoBuyandSellStock/solution.rb
@@ -0,0 +1,12 @@
+# @param {Integer[]} prices
+# @return {Integer}
+def max_profit(prices)
+  max_profit = 0
+  len = prices.length
+  for i in 0..(len-2)
+    max = prices[i+1..len-1].max
+    diff = max - prices[i]
+    max_profit = diff if diff > max_profit && diff.positive?
+  end
+  max_profit
+end
