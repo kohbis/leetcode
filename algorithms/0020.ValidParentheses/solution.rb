aeb841383267b8869ab129fb4a3e34bfccce14a6
Kohei Sugimoto <18735471+kohbis@users.noreply.github.com>
2020-02-04 00:04:02 +0900

    solved 0020.ValidParentheses

---
+++ algorithms/0020.ValidParentheses/solution.rb
@@ -0,0 +1,18 @@
+# @param {String} s
+# @return {Boolean}
+def is_valid(s)
+  stack = []
+  brackets = { '(' => ')', '{' => '}', '[' => ']' }
+  
+  s.each_char do |c|
+    if brackets.has_key?(c)
+      stack.push(c)
+    elsif brackets.has_value?(c)
+      return false if brackets[stack.pop] != c
+    else
+      return false
+    end
+  end
+  
+  stack.empty?
+end
