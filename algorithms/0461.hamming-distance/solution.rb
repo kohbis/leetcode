0a1aafd81ebe287d36004f0e4a2713419f061255
Kohei Sugimoto <18735471+kohbis@users.noreply.github.com>
2020-03-10 00:54:02 +0900

    solved 461

---
+++ algorithms/0461.hamming-distance/solution.rb
@@ -0,0 +1,8 @@
+# @param {Integer} x
+# @param {Integer} y
+# @return {Integer}
+def hamming_distance(x, y)
+  x_s = x.to_s(2).rjust(32, '0')
+  y_s = y.to_s(2).rjust(32, '0')
+  (0..31).select {|i| x_s[i] != y_s[i] }.count
+end
