a8139c3126603d1eca07bb4e391b19f6530e3441
Kohei Sugimoto <18735471+kohbis@users.noreply.github.com>
2020-04-04 23:57:02 +0900

    upd 121 range

--- algorithms/0121.best-time-to-buy-and-sell-stock/solution.rb
+++ algorithms/0121.best-time-to-buy-and-sell-stock/solution.rb
@@ -4,7 +4,7 @@ def max_profit(prices)
   max_profit = 0
   len = prices.length
   for i in 0..(len-2)
-    max = prices[i+1..len-1].max
+    max = prices[i+1...len].max
     diff = max - prices[i]
     max_profit = diff if diff > max_profit && diff.positive?
   end
