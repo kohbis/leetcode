141b2cfc398fbe21ffbaeef3d2eba74980097c96
Kohei Sugimoto <18735471+kohbis@users.noreply.github.com>
2020-04-12 23:13:36 +0900

    add 0136 second solutino in ruby

--- algorithms/0136.single-number/solution.rb
+++ algorithms/0136.single-number/solution.rb
@@ -1,5 +1,17 @@
 # @param {Integer[]} nums
 # @return {Integer}
 def single_number(nums)
+  ## XOR ##
   nums.reduce(&:^)
+
+  ## EACH ##
+  # temp = []
+  # nums.each do |n|
+  #   if temp.include?(n)
+  #     temp.delete(n)
+  #   else
+  #     temp << n
+  #   end
+  # end
+  # temp[0]
 end
