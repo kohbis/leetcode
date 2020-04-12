ac2c85b9d5e7bddccb01f8b34f62a99937a32f86
Kohei Sugimoto <18735471+kohbis@users.noreply.github.com>
2020-04-13 00:15:57 +0900

    solve 283

---
+++ algorithms/0283.move-zeroes/solution.rb
@@ -0,0 +1,13 @@
+# @param {Integer[]} nums
+# @return {Void} Do not return anything, modify nums in-place instead.
+def move_zeroes(nums)
+  cnt = 0
+  nums.each_with_index do |n, idx|
+    if n.zero?
+      cnt += 1
+    else
+      nums[idx] = 0
+      nums[idx-cnt] = n
+    end
+  end
+end
