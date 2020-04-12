ffc3276516a51f0e5e3be04da834c2840f49b9f8
Kohei Sugimoto <18735471+kohbis@users.noreply.github.com>
2020-04-12 22:33:48 +0900

    solve 202

---
+++ algorithms/0202.happy-number/solution.rb
@@ -0,0 +1,13 @@
+# @param {Integer} n
+# @return {Boolean}
+def is_happy(n)
+  arr = []
+  until arr.include?(n)
+    sum = 0
+    n.to_s.chars.map(&:to_i).each { |x| sum += x*x }
+    return true if sum == 1
+    arr << n
+    n = sum
+  end
+  false
+end
