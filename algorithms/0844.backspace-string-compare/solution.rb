fdf0021766632f220cb5f89f3a91d0c61e9240c3
Kohei Sugimoto <18735471+kohbis@users.noreply.github.com>
2020-04-19 01:59:59 +0900

    solve 155&844

---
+++ algorithms/0844.backspace-string-compare/solution.rb
@@ -0,0 +1,19 @@
+def string_builder(str)
+  arr = []
+  str.chars.each do |char|
+    if '#' == char
+      arr.pop
+    else
+      arr.push(char)
+    end
+  end
+  arr.join
+end
+
+# @param {String} s
+# @param {String} t
+# @return {Boolean}
+def backspace_compare(s, t)
+  string_builder(s) == string_builder(t)
+end
+
