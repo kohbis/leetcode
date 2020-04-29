f50a095e5ece4acfea191024247fc36ec1757414
Kohei Sugimoto <18735471+kohbis@users.noreply.github.com>
2020-04-30 00:35:49 +0900

    solve 525

---
+++ algorithms/0525.contiguous-array/solution.rb
@@ -0,0 +1,16 @@
+# @param {Integer[]} nums
+# @return {Integer}
+def find_max_length(nums)
+  res = 0
+  cnt, h = 0, {0 => -1}
+  nums.each_with_index do |num, idx|
+    cnt += num == 0 ? -1 : 1
+    if h.has_key?(cnt)
+      res = [res, idx - h[cnt]].max
+    else
+      h[cnt] = idx
+    end
+  end
+  res
+end
+
