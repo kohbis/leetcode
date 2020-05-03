dcbbd73851d16b7ab3cdab49620505a014483b3b
Kohei Sugimoto <18735471+kohbis@users.noreply.github.com>
2020-05-03 20:00:56 +0900

    upd 771

--- algorithms/0771.jewels-and-stones/solution.rb
+++ algorithms/0771.jewels-and-stones/solution.rb
@@ -2,7 +2,11 @@
 # @param {String} s
 # @return {Integer}
 def num_jewels_in_stones(j, s)
-  cnt = 0
-  j.chars.map { |c| cnt += s.count(c) }
-  cnt
+
+  # cnt = 0
+  # j.chars.map { |c| cnt += s.count(c) }
+  # cnt
+
+  j.chars.inject(0) {|ans, c| ans += s.count(c) }
 end
+
