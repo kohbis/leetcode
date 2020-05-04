39e97fe96ab6a83099ad43e8ae32d06ad095c37a
Kohei Sugimoto <18735471+kohbis@users.noreply.github.com>
2020-05-05 00:30:13 +0900

    upd 476

--- algorithms/0476.number-complement/solution.rb
+++ algorithms/0476.number-complement/solution.rb
@@ -1,6 +1,8 @@
 # @param {Integer} num
 # @return {Integer}
 def find_complement(num)
-  num.to_s(2).chars.map {|c| c == '0' ? '1' : '0' }.join.to_i(2)
+  # num.to_s(2).chars.map {|c| c == '0' ? '1' : '0' }.join.to_i(2)
+
+  num.to_s(2).tr('01', '10').to_i(2)
 end
 
