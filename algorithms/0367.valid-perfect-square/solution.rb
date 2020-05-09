56d2315ca0bb37877b43b9b9720242fcb79294a6
Kohei Sugimoto <18735471+kohbis@users.noreply.github.com>
2020-05-09 20:25:55 +0900

    solve 367

---
+++ algorithms/0367.valid-perfect-square/solution.rb
@@ -0,0 +1,13 @@
+# @param {Integer} num
+# @return {Boolean}
+def is_perfect_square(num)
+  r = num == 1 ? 1 : num/2
+  while r*r > num
+    # x0*2 = (x0**2 -k) / (x0 -x1)
+    # x1 = x0 - (x0**2 - k) / x0*2
+    #    = (x0**2 + k) / x0*2
+    #    = (x0 + k/x0) / 2
+    r = (r + num/r) / 2
+  end
+  r*r == num
+end
