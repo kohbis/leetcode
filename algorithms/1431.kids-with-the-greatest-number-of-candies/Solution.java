0f4d609fb726c074da6b3daaca80bcdeab4514a4
Kohei Sugimoto <18735471+kohbis@users.noreply.github.com>
2020-05-16 21:50:47 +0900

    solve 1431 in java

---
+++ algorithms/1431.kids-with-the-greatest-number-of-candies/Solution.java
@@ -0,0 +1,18 @@
+class Solution {
+    public List<Boolean> kidsWithCandies(int[] candies, int extraCandies) {
+        int maxCandies = 0;
+        List<Boolean> res = new ArrayList<Boolean>(candies.length);   
+
+        for(int candy : candies) {
+            if (candy > maxCandies) {
+                maxCandies = candy;
+            }
+        }
+        
+        for(int candy : candies) {
+            res.add(candy + extraCandies >= maxCandies);
+        }
+
+        return res;
+    }
+}
