4123a75d5efac2fdda5b58cc73c3819c9729b36a
Kohei Sugimoto <18735471+kohbis@users.noreply.github.com>
2020-06-06 23:10:17 +0900

    update 1464 in ruby

--- algorithms/1464.maximum-product-of-two-elements-in-an-array/solution.rb
+++ algorithms/1464.maximum-product-of-two-elements-in-an-array/solution.rb
@@ -1,5 +1,16 @@
 # @param {Integer[]} nums
 # @return {Integer}
 def max_product(nums)
-  nums.max(2).inject(1) { |res, num| res * (num - 1) }
+  # nums.max(2).inject(1) { |res, num| res * (num - 1) }
+  
+  max, second_max = 0, 0
+  nums.each do |num|
+    if num > max then
+      second_max = max
+      max = num
+    elsif num > second_max then
+      second_max = num
+    end
+  end
+  (max - 1) * (second_max - 1)
 end
