135ec85222dc9685043e272de39b5e2d88344efa
Kohei Sugimoto <18735471+kohbis@users.noreply.github.com>
2020-07-01 23:02:44 +0900

    solve 1480

---
+++ algorithms/1480.running-sum-of-1d-array/solution.rs
@@ -0,0 +1,12 @@
+impl Solution {
+    pub fn running_sum(nums: Vec<i32>) -> Vec<i32> {
+        let mut sum = 0;
+        nums
+            .into_iter()
+            .map (|num| {
+                sum += num;
+                sum
+            })
+            .collect()
+    }
+}
