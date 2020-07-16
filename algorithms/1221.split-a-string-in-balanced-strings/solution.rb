19f2c3f02a9de2ae65bfbd616fbe26211b41cb0e
Kohei Sugimoto <18735471+kohbis@users.noreply.github.com>
2020-07-16 23:51:00 +0900

    solve 1221

---
+++ algorithms/1221.split-a-string-in-balanced-strings/solution.rb
@@ -0,0 +1,14 @@
+# @param {String} s
+# @return {Integer}
+def balanced_string_split(s)
+  res, cnt = 0, 0
+  s.chars.each do |c|
+    if c == "L"
+      cnt += 1
+    else
+      cnt -= 1
+    end
+    res += 1 if cnt == 0 
+  end
+  res
+end
