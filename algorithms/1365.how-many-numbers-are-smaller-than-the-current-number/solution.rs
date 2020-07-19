93a643c123f3df4cf9afd366a90e75c07fd37bb2
Kohei Sugimoto <18735471+kohbis@users.noreply.github.com>
2020-07-19 21:41:51 +0900

    solve 1365 in rust

---
+++ algorithms/1365.how-many-numbers-are-smaller-than-the-current-number/solution.rs
@@ -0,0 +1,8 @@
+impl Solution {
+    pub fn smaller_numbers_than_current(nums: Vec<i32>) -> Vec<i32> {
+        nums
+            .iter()
+            .map (|n| nums.iter().filter(|&x| n > x).count() as i32)
+            .collect()
+    }
+}
