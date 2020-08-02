057a6b10ffe9f56caa8ccf144febaebd26e99ed4
Kohei Sugimoto <18735471+kohbis@users.noreply.github.com>
2020-08-02 11:07:11 +0900

    solve 520 in ruby,rust

---
+++ algorithms/0520.detect-capital/solutino.rb
@@ -0,0 +1,16 @@
+# @param {String} word
+# @return {Boolean}
+def detect_capital_use(word)
+  # ruby has useful methods!!!
+  # word == word.capitalize || word == word.upcase || word == word.downcase
+
+  uppers = word.chars.count {|c| c.is_upper? }
+  uppers == 0 || uppers == word.length || (uppers == 1 && word[0].is_upper? )
+end
+
+class String
+  def is_upper?
+    self >= 'A' && 'Z' >= self
+  end
+end
+
---
+++ algorithms/0520.detect-capital/solutino.rs
@@ -0,0 +1,8 @@
+impl Solution {
+    pub fn detect_capital_use(word: String) -> bool {
+        let v_word: Vec<char> = word.chars().collect();
+        let uppers = v_word.iter().filter(|&&c| c.is_uppercase()).cloned().collect::<Vec<char>>().len();
+        uppers == 0 || uppers == word.len() || (uppers == 1 && v_word[0].is_uppercase())
+    }
+}
+
