1aeb5ab6db75359b02c6ce52f6ed0593be489daf
Kohei Sugimoto <18735471+kohbis@users.noreply.github.com>
2020-08-05 15:26:08 +0900

    update 342 in ruby

--- algorithms/0342.power-of-four/solution.rb
+++ algorithms/0342.power-of-four/solution.rb
@@ -1,12 +1,16 @@
+
 # @param {Integer} num
 # @return {Boolean}
 def is_power_of_four(num)
-  if num == 0
-    false
-  elsif num == 1
-    true
-  else
-    num % 4 == 0 && is_power_of_four(num / 4)
-  end
+  # if num == 0
+  #   false
+  # elsif num == 1
+  #   true
+  # else
+  #   num % 4 == 0 && is_power_of_four(num / 4)
+  # end
+
+  b = num.to_s(2)
+  b[0] == "1" && b[1..-1].length.even? && b[1..-1].count("1") == 0
 end
 
