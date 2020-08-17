b1275a09aa2a8a6b1c0bc15f38cea2b60db1c7fb
Kohei Sugimoto <18735471+kohbis@users.noreply.github.com>
2020-08-17 21:20:37 +0900

    solve 69,1207 in ruby

---
+++ algorithms/0069.sqrtx/solution.rb
@@ -0,0 +1,6 @@
+# @param {Integer} x
+# @return {Integer}
+def my_sqrt(x)
+  Math.sqrt(x).floor
+end
+
---
+++ algorithms/1207.unique-number-of-occurrences/solution.rb
@@ -0,0 +1,8 @@
+# @param {Integer[]} arr
+# @return {Boolean}
+def unique_occurrences(arr)
+  counts = Hash.new(0)
+  arr.each {|i| counts[i] += 1 }
+  counts.values.size == counts.values.uniq.size
+end
+
