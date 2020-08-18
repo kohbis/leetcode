34ff744a16ef997dfd8f46fa2ea17b5490781e42
Kohei Sugimoto <18735471+kohbis@users.noreply.github.com>
2020-08-18 12:34:59 +0900

    solve 70 in ruby

---
+++ algorithms/0070.climbing-stairs/solution.rb
@@ -0,0 +1,14 @@
+# @param {Integer} n
+# @return {Integer}
+def climb_stairs(n)
+  # fibonacci sequence
+
+  # a = b = 1
+  # n.times {|i| a, b = b, a+b }
+  # a
+
+  a = [1, 1]
+  (2..n).each {|i| a[i] = a[i-2] + a[i-1] }
+  a[-1]
+end
+
