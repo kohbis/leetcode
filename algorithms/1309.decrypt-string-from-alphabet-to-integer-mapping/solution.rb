70786fc3008fe6eaf8d8524254055b50872feaac
Kohei Sugimoto <18735471+kohbis@users.noreply.github.com>
2020-08-18 20:49:00 +0900

    solve 1309 in ruby

---
+++ algorithms/1309.decrypt-string-from-alphabet-to-integer-mapping/solution.rb
@@ -0,0 +1,14 @@
+Alph = *('a'..'z')
+
+# @param {String} s
+# @return {String}
+def freq_alphabets(s)
+  res, chars = [], s.chars
+  until chars.size == 0
+    c = chars.pop
+    idx = (c == '#' ? chars.pop(2).join.to_i : c.to_i) - 1
+    res.unshift Alph[idx]
+  end
+  res.join
+end
+
