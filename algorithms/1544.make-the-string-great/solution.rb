559ed6460ded4c01213dee2676a1d5111f458ffa
Kohei Sugimoto <18735471+kohbis@users.noreply.github.com>
2020-08-23 12:33:06 +0900

    solve 1544 in ruby

---
+++ algorithms/1544.make-the-string-great/solution.rb
@@ -0,0 +1,24 @@
+# @param {String} s
+# @return {String}
+def make_good(s)
+  stack = []
+  s.chars.each do |c|
+    if !stack.empty? && ((stack[-1].is_upper? && stack[-1].downcase == c) || (stack[-1].is_lower? && stack[-1].upcase == c))
+      stack.pop
+    else
+      stack.push(c)
+    end
+  end
+  stack.join
+end
+
+class String
+  def is_upper?
+    self >= 'A' && 'Z' >= self
+  end
+  
+  def is_lower?
+    self >= 'a' && 'z' >= self
+  end
+end
+
