144dc9500af9f89837fd46eab17a68fbbd8bad99
Kohei Sugimoto <18735471+kohbis@users.noreply.github.com>
2020-08-25 00:14:33 +0900

    solve 1512 in rust

---
+++ algorithms/1512.number-of-good-pairs/solution.rs
@@ -0,0 +1,15 @@
+impl Solution {
+    pub fn num_identical_pairs(nums: Vec<i32>) -> i32 {
+        let mut res: i32 = 0;
+        for i in 0..nums.len() {
+            for j in i..nums.len() {
+                if nums[i] == nums[j] && i < j {
+                    res += 1;
+                }
+            }
+        }
+        
+        res
+    }
+}
+
