0aba36615fcc15e7da4108b8570e486439f5c9b6
Kohei Sugimoto <18735471+kohbis@users.noreply.github.com>
2020-08-26 08:58:28 +0900

    solve 1556 in rust

---
+++ algorithms/1556.thousand-separator/solution.rs
@@ -0,0 +1,15 @@
+impl Solution {
+    pub fn thousand_separator(n: i32) -> String {
+        let mut res = String::new();
+        let s_n = n.to_string();
+        for (idx, c) in s_n.chars().rev().enumerate() {
+            res.insert(0, c);
+            if (idx + 1) % 3 == 0 && (idx + 1) != s_n.len() {
+                res.insert(0, '.');
+            }
+        }
+        
+        res
+    }
+}
+
