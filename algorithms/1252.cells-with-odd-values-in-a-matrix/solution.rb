b23dc4dfb927728eafd68bf333a7c2bc90d5bbdd
Kohei Sugimoto <18735471+kohbis@users.noreply.github.com>
2020-09-02 08:34:02 +0900

    solve 1252 in ruby

---
+++ algorithms/1252.cells-with-odd-values-in-a-matrix/solution.rb
@@ -0,0 +1,22 @@
+
+def odd_cells(n, m, indices)
+  matrix = Array.new(n) { Array.new(m, 0) }
+  count = 0
+
+  # apply the increment to all indices
+  indices.each do |ind|
+    ri, ci = ind[0], ind[1]
+    # row
+    matrix[ri].map! {|cell| cell = cell + 1 }
+    # column
+    matrix.each {|row| row[ci] = row[ci] + 1 }
+  end
+
+  # matrix.flatten.count {|cell| cell % 2 != 0 }
+  matrix.each do |row|
+    count += row.count {|cell| cell % 2 != 0 }
+  end
+
+  count
+end
+
