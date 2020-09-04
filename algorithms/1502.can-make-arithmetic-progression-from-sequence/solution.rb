f70af97d390d5752b1cb4437af3c363eedbf6dd8
Kohei Sugimoto <18735471+kohbis@users.noreply.github.com>
2020-09-05 00:08:10 +0900

    solve 1502 in rust, ruby

---
+++ algorithms/1502.can-make-arithmetic-progression-from-sequence/solution.rb
@@ -0,0 +1,15 @@
+# @param {Integer[]} arr
+# @return {Boolean}
+def can_make_arithmetic_progression(arr)
+  ## one liner ##
+  # arr.sort.each_cons(2).map {|a, b| a - b }.uniq.size == 1
+
+  ## calc each combinations ##
+  arr.sort!
+  diff = arr[0] - arr[1]
+  (1...arr.size-1).each do |i|
+    return false unless diff == arr[i] - arr[i+1]
+  end
+  # default
+  true
+end
---
+++ algorithms/1502.can-make-arithmetic-progression-from-sequence/solution.rs
@@ -0,0 +1,20 @@
+impl Solution {
+    pub fn can_make_arithmetic_progression(arr: Vec<i32>) -> bool {
+        let mut reorder = arr.clone();
+        reorder.sort();
+
+        // using HashSet //
+        // use std::collections::HashSet;
+        // (0..(arr.len() - 1)).map(|i| reorder[i] - reorder[i + 1]).collect::<HashSet<i32>>().len() == 1
+
+        // calc each conbinations //
+        let diff = reorder[0] - reorder[1];
+        for i in 1..(arr.len() - 1) {
+            if diff != reorder[i] - reorder[i + 1] {
+                return false
+            }
+        }
+
+        true
+    }
+}
