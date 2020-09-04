49cd8ddb0c7e961faec7b3d4ee6703baeb9b7ae4
Kohei Sugimoto <18735471+kohbis@users.noreply.github.com>
2020-09-04 20:21:56 +0900

    solve 1370 in rust

---
+++ algorithms/1370.increasing-decreasing-string/solution.rs
@@ -0,0 +1,32 @@
+impl Solution {
+    pub fn sort_string(s: String) -> String {
+        // result
+        let mut res: Vec<char> = Vec::new();
+        // sorted charactors
+        let mut seeds: Vec<char> = s.chars().collect();
+        seeds.sort();
+        // reverse flag
+        let mut rev: bool = false;
+
+        while seeds.len() > 0 {
+            let mut additions = seeds.clone();
+            additions.dedup();
+
+            if rev {
+                additions = additions.into_iter().rev().collect();
+            };
+
+            for c in additions.into_iter() {
+                res.push(c);
+
+                let index = seeds.iter().position(|x| *x == c ).unwrap();
+                seeds.remove(index);
+            }
+
+            // toggle
+            rev = !rev;
+        }
+
+        res.iter().collect()
+    }
+}
