cd95f1408c198d51ce7ff0674f7c0abe8ea49608
Kohei Sugimoto <18735471+kohbis@users.noreply.github.com>
2020-09-08 00:16:03 +0900

    solve 567 in rust & ruby

---
+++ algorithms/0657.robot-return-to-origin/solution.rb
@@ -0,0 +1,6 @@
+# @param {String} moves
+# @return {Boolean}
+def judge_circle(moves)
+  moves.count("L") == moves.count("R") &&
+  moves.count("U") == moves.count("D")
+end
---
+++ algorithms/0657.robot-return-to-origin/solution.rs
@@ -0,0 +1,18 @@
+impl Solution {
+    pub fn judge_circle(moves: String) -> bool {
+        use std::collections::HashMap;
+
+        let directions: Vec<char> = vec!['L', 'R', 'U', 'D'];
+        let sequences: Vec<char> = moves.chars().collect();
+
+        let mut map: HashMap<char, i32> = HashMap::new();
+
+        for d in directions.iter() {
+            let counts = sequences.iter().filter(|&x| x == d).count() as i32;
+            map.insert(*d, counts);
+        }
+
+        map.get(&'L') == map.get(&'R')
+        && map.get(&'U') == map.get(&'D')
+    }
+}
