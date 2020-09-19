6b1c3322ae9786db3a36d90af313610856649edb
Kohei Sugimoto <18735471+kohbis@users.noreply.github.com>
2020-09-19 15:00:27 +0900

    solve 344 in rust & ruby

---
+++ algorithms/0344.reverse-string/solution.rb
@@ -0,0 +1,15 @@
+# @param {Character[]} s
+# @return {Void} Do not return anything, modify s in-place instead.
+def reverse_string(s)
+  # It's lazy
+  # s.reverse!
+
+  # in-place 
+  i, j = 0, s.size-1
+  while i < j
+    s[i], s[j] = s[j], s[i]
+    i += 1
+    j -= 1
+  end
+  s
+end
