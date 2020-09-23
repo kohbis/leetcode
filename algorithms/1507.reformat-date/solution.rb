4b1d54982887c963aaaa1760d9b377cb57eba48b
Kohei Sugimoto <18735471+kohbis@users.noreply.github.com>
2020-09-24 08:57:58 +0900

    solve 1507 in rust & ruby

---
+++ algorithms/1507.reformat-date/solution.rb
@@ -0,0 +1,17 @@
+# @param {String} date
+# @return {String}
+def reformat_date(date)
+  ## use Date ##
+  # require 'date'
+  # Date.parse(date).to_s
+
+  year, month, day = date.split(" ").reverse
+
+  months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
+  month = (months.find_index {|m| m == month } + 1).to_s.rjust(2, "0")
+
+  # remove st, nd, rd, th
+  day = day.tr("dhnrst", "").rjust(2, "0")
+
+  [year, month, day].join("-")
+end
