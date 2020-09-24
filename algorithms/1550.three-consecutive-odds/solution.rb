478f388077e12297a4b3146f4470700bbf42dec0
Kohei Sugimoto <18735471+kohbis@users.noreply.github.com>
2020-09-25 07:17:55 +0900

    solve 1550 in rust & ruby

---
+++ algorithms/1550.three-consecutive-odds/solution.rb
@@ -0,0 +1,8 @@
+# @param {Integer[]} arr
+# @return {Boolean}
+def three_consecutive_odds(arr)
+  arr.each_cons(3) do |a, b, c|
+    return true if [a, b, c].count {|i| i % 2 == 0 } == 0
+  end
+  false
+end
---
+++ algorithms/1550.three-consecutive-odds/solution.rs
@@ -0,0 +1,10 @@
+impl Solution {
+    pub fn three_consecutive_odds(arr: Vec<i32>) -> bool {
+        for w in arr.windows(3) {
+            if [w[0], w[1], w[2]].iter().filter(|&i| i % 2 == 0).count() == 0 {
+                return true
+            }
+        }
+        false
+    }
+}
