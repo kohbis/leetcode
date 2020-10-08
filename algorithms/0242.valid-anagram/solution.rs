44b53dba01e84c8c77fdaebe5cbbc754b178b1fe
Kohei Sugimoto <18735471+kohbis@users.noreply.github.com>
2020-10-08 23:56:50 +0900

    solve 242 in rust & ruby

---
+++ algorithms/0242.valid-anagram/solution.rs
@@ -0,0 +1,11 @@
+impl Solution {
+    pub fn is_anagram(s: String, t: String) -> bool {
+        let mut s: Vec<char> = s.chars().collect();
+        let mut t: Vec<char> = t.chars().collect();
+
+        s.sort();
+        t.sort();
+
+        s == t
+    }
+}
