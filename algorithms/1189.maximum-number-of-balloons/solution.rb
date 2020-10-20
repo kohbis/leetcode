074af82163de157864cad697b99c8474dd2e6a7f
Kohei Sugimoto <18735471+kohbis@users.noreply.github.com>
2020-10-20 23:13:19 +0900

    solve 1189 in ruby

---
+++ algorithms/1189.maximum-number-of-balloons/solution.rb
@@ -0,0 +1,6 @@
+# @param {String} text
+# @return {Integer}
+def max_number_of_balloons(text)
+  arr = text.chars
+  [arr.count("b"), arr.count("a"), (arr.count("l") / 2), (arr.count("o") / 2), arr.count("n")].min
+end
