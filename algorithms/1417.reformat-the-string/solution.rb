9a41277ef488b372e94605fd038042c9e6b5c872
Kohei Sugimoto <18735471+kohbis@users.noreply.github.com>
2020-10-22 23:42:17 +0900

    solve 1417 in ruby

---
+++ algorithms/1417.reformat-the-string/solution.rb
@@ -0,0 +1,31 @@
+# @param {String} s
+# @return {String}
+def reformat(s)
+  alpha = []
+  nums = []
+  res = ""
+
+  s.chars.each do |c|
+    if /[[:alpha:]]/.match(c)
+      alpha << c
+    else
+      nums << c
+    end
+  end
+
+  return "" if (alpha.size - nums.size).abs > 1
+
+  if alpha.size > nums.size
+    while alpha.any?
+      res << alpha.pop
+      res << nums.pop unless nums.empty?
+    end
+  else
+    while nums.any?
+      res << nums.pop
+      res << alpha.pop unless alpha.empty?
+    end
+  end
+
+  res
+end
