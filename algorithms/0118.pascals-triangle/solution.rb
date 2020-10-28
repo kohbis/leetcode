e63d0f856cc6ea014b2ec6f91b6af73e2df22468
Kohei Sugimoto <18735471+kohbis@users.noreply.github.com>
2020-10-28 22:24:00 +0900

    solve 118 in rust & ruby

---
+++ algorithms/0118.pascals-triangle/solution.rb
@@ -0,0 +1,18 @@
+# @param {Integer} num_rows
+# @return {Integer[][]}
+def generate(num_rows)
+  res = []
+
+  # generate
+  num_rows.times do |i|
+    # init row
+    res << [1] * (i + 1)
+
+    # calculation
+    (1...i).each do |j|
+      res[i][j] = res[i-1][j-1] + res[i-1][j]
+    end
+  end
+
+  res
+end
---
+++ algorithms/0118.pascals-triangle/solution.rs
@@ -0,0 +1,17 @@
+impl Solution {
+    pub fn generate(num_rows: i32) -> Vec<Vec<i32>> {
+        let mut res: Vec<Vec<i32>> = Vec::new();
+
+        for i in 0..num_rows as usize {
+            // init row
+            res.push(vec![1; i+1]);
+
+            // calculation
+            for j in 1..i {
+                res[i][j] = res[i-1][j-1] + res[i-1][j];
+            }
+        }
+
+        res
+    }
+}
