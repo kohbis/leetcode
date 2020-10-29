a045e9d8ecde52ac4aae8fad7039b1c5d74e8796
Kohei Sugimoto <18735471+kohbis@users.noreply.github.com>
2020-10-29 23:50:14 +0900

    solve 119 in rust & ruby

---
+++ algorithms/0119.pascals-triangle-ii/solution.rb
@@ -0,0 +1,18 @@
+# @param {Integer} row_index
+# @return {Integer[]}
+def get_row(row_index)
+  rows = [[1]]
+
+  # generate
+  1.upto(row_index) do |i|
+    # init row
+    rows << [1] * (i + 1)
+
+    # calculation
+    (1...i).each do |j|
+      rows[i][j] = rows[i-1][j-1] + rows[i-1][j]
+    end
+  end
+
+  rows[-1]
+end
---
+++ algorithms/0119.pascals-triangle-ii/solution.rs
@@ -0,0 +1,17 @@
+impl Solution {
+    pub fn get_row(row_index: i32) -> Vec<i32> {
+        let mut rows: Vec<Vec<i32>> = vec![vec![1]];
+
+        for i in 1..=row_index as usize {
+            // init row
+            rows.push(vec![1; i+1]);
+
+            // calculation
+            for j in 1..i {
+                rows[i][j] = rows[i-1][j-1] + rows[i-1][j];
+            }
+        }
+
+        rows.last().unwrap().to_vec()
+    }
+}
