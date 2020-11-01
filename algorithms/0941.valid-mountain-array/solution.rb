e3823707faca253e17472514536ceba04ef42bf6
Kohei Sugimoto <18735471+kohbis@users.noreply.github.com>
2020-11-01 12:53:48 +0900

    solve,update 941 in rust,ruby

--- algorithms/0941.valid-mountain-array/solution.rb
+++ algorithms/0941.valid-mountain-array/solution.rb
@@ -1,13 +1,35 @@
 # @param {Integer[]} a
 # @return {Boolean}
 def valid_mountain_array(a)
-  return false unless a.size >= 3
-  return false unless a.count(a.max) == 1
+  return false if a.size < 3
 
-  top = a.find_index(a.max)
-  left, right = a[...top], a[top+1..]
+  i = 1
+  while i < a.size
+    break if a[i] <= a[i-1]
+    i += 1
+  end
+  
+  return false if i == a.size || i == 1
+  
+  while i < a.size
+    return false if a[i] >= a[i-1]
+    i += 1
+  end
 
-  return false if left.empty? || right.empty?
-
-  (left == left.sort.uniq) && (right == right.sort.uniq.reverse)
+  true
 end
+
+
+# # @param {Integer[]} a
+# # @return {Boolean}
+# def valid_mountain_array(a)
+#   return false unless a.size >= 3
+#   return false unless a.count(a.max) == 1
+
+#   top = a.find_index(a.max)
+#   left, right = a[...top], a[top+1..]
+
+#   return false if left.empty? || right.empty?
+
+#   (left == left.sort.uniq) && (right == right.sort.uniq.reverse)
+# end
---
+++ algorithms/0941.valid-mountain-array/solution.rs
@@ -0,0 +1,28 @@
+impl Solution {
+    pub fn valid_mountain_array(a: Vec<i32>) -> bool {
+        if a.len() < 3 {
+            return false;
+        }
+
+        let mut i: usize = 1;
+        while i < a.len() {
+            if a[i] <= a[i-1] {
+                break;
+            }
+            i += 1;
+        }
+
+        if i == a.len() || i == 1 {
+            return false;
+        }
+
+        while i < a.len() {
+            if a[i] >= a[i-1] {
+                return false;
+            }
+            i += 1;
+        }
+
+        true
+    }
+}
