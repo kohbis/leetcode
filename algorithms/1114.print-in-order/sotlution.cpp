92ec7a7db58a46ec231d41f49f0d244f400861a8
Kohei Sugimoto <18735471+kohbis@users.noreply.github.com>
2020-11-08 16:18:57 +0900

    solve 1114 in java & c++

---
+++ algorithms/1114.print-in-order/sotlution.cpp
@@ -0,0 +1,31 @@
+#include <mutex>
+
+class Foo {
+    mutex mtx_second;
+    mutex mtx_third;
+public:
+    Foo() {
+        mtx_second.lock();
+        mtx_third.lock();
+    }
+
+    void first(function<void()> printFirst) {
+        
+        // printFirst() outputs "first". Do not change or remove this line.
+        printFirst();
+        mtx_second.unlock();
+    }
+
+    void second(function<void()> printSecond) {
+        mtx_second.lock();
+        // printSecond() outputs "second". Do not change or remove this line.
+        printSecond();
+        mtx_third.unlock();
+    }
+
+    void third(function<void()> printThird) {
+        mtx_third.lock();
+        // printThird() outputs "third". Do not change or remove this line.
+        printThird();
+    }
+};
