de5e99908558e08d9650a78cbc87edc0c3489342
Kohei Sugimoto <18735471+kohbis@users.noreply.github.com>
2020-11-15 00:33:48 +0900

    solve 1408 in rust & ruby

---
+++ algorithms/1408.string-matching-in-an-array/solution.rb
@@ -0,0 +1,17 @@
+# @param {String[]} words
+# @return {String[]}
+def string_matching(words)
+  res = []
+  words = words.sort_by(&:length)
+  
+  (0...words.size-1).each do |i|
+    (i+1...words.size).each do |j|
+      if words[j][words[i]]
+        res << words[i]
+        break
+      end
+    end
+  end
+
+  res
+end
---
+++ algorithms/1408.string-matching-in-an-array/solution.rs
@@ -0,0 +1,18 @@
+impl Solution {
+    pub fn string_matching(words: Vec<String>) -> Vec<String> {
+        let mut res: Vec<String> = vec![];
+        let mut words = words.clone();
+        words.sort_by(|a, b| a.len().partial_cmp(&b.len()).unwrap());
+
+        for i in 0..words.len()-1 {
+            for j in i+1..words.len() {
+                if words[j].contains(&words[i]) {
+                    res.push(words[i].clone());
+                    break
+                }
+            }
+        }
+
+        res
+    }
+}
