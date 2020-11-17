8a887dde6fa459d6c7f00b1a3e85ffdbd86515f2
Kohei Sugimoto <18735471+kohbis@users.noreply.github.com>
2020-11-17 22:33:59 +0900

    solve 1346 in rust & ruby

---
+++ algorithms/1346.check-if-n-and-its-double-exist/solution.rb
@@ -0,0 +1,14 @@
+# @param {Integer[]} arr
+# @return {Boolean}
+def check_if_exist(arr)
+  hash = {}
+  arr.each do |i|
+    return true if hash[i*2]
+    return true if i%2 == 0 && hash[i/2]
+
+    hash[i] = true
+  end
+
+  # default
+  false
+end
---
+++ algorithms/1346.check-if-n-and-its-double-exist/solution.rs
@@ -0,0 +1,25 @@
+impl Solution {
+    pub fn check_if_exist(arr: Vec<i32>) -> bool {
+        use std::collections::HashMap;
+        let mut map: HashMap<i32, bool> = HashMap::new();
+
+        for i in arr {
+            if map.contains_key(&(i*2)) {
+                if map[&(i*2)] {
+                    return true;
+                }
+            }
+
+            if (i%2 == 0) && map.contains_key(&(i/2)) {
+                if map[&(i/2)] {
+                    return true;
+                }
+            }
+
+            map.insert(i, true);
+        }
+
+        // default
+        false
+    }
+}
