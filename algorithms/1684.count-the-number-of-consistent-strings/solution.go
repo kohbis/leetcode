7249024d43a813eaa7934b770d6163c5d35146ec
Kohei Sugimoto <18735471+kohbis@users.noreply.github.com>
2020-12-22 22:40:25 +0900

    gofmt

--- algorithms/1684.count-the-number-of-consistent-strings/solution.go
+++ algorithms/1684.count-the-number-of-consistent-strings/solution.go
@@ -1,20 +1,20 @@
 func countConsistentStrings(allowed string, words []string) int {
-    count := 0
+	count := 0
 
-    for _, word := range words {
-        onlyAllowedChars := true
+	for _, word := range words {
+		onlyAllowedChars := true
 
-        for _, c := range word {
-            if !strings.ContainsRune(allowed, c) {
-                onlyAllowedChars = false
-                break
-            }
-        }
+		for _, c := range word {
+			if !strings.ContainsRune(allowed, c) {
+				onlyAllowedChars = false
+				break
+			}
+		}
 
-        if onlyAllowedChars {
-            count += 1
-        }
-    }
+		if onlyAllowedChars {
+			count += 1
+		}
+	}
 
-    return count
+	return count
 }
