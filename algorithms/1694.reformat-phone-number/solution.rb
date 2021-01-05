543ee3e256c26380a03566bd408b9d1d9a3a31fa
Kohei Sugimoto <18735471+kohbis@users.noreply.github.com>
2021-01-05 23:09:30 +0900

    solve 1694 in ruby

---
+++ algorithms/1694.reformat-phone-number/solution.rb
@@ -0,0 +1,28 @@
+# @param {String} number
+# @return {String}
+def reformat_number(number)
+  arr = []
+  nums = number.gsub(/-| /, "").chars
+
+  three_digits = nums.size / 3
+  two_digits = case nums.size % 3
+  when 0 then
+    0
+  when 1 then
+    three_digits -= 1
+    2
+  when 2
+    1
+  end
+
+  three_digits.times do
+    arr << nums.shift(3).join
+  end
+
+  two_digits.times do
+    arr << nums.shift(2).join
+  end
+
+  arr.join("-")
+end
+
