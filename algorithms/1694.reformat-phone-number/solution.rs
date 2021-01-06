e90c3dbf5338fd07cc0cf4bbe77c3c9fb5d7ed4f
Kohei Sugimoto <18735471+kohbis@users.noreply.github.com>
2021-01-07 00:16:59 +0900

    solve 1694 in rust

---
+++ algorithms/1694.reformat-phone-number/solution.rs
@@ -0,0 +1,36 @@
+impl Solution {
+    pub fn reformat_number(number: String) -> String {
+        use std::collections::VecDeque;
+
+        let mut parts: Vec<String> = vec![];
+        let mut nums: VecDeque<char> = number.clone().replace("-", "").replace(" ", "").chars().collect();
+
+        let mut three_digits = nums.len() / 3;
+        let mut two_digits = match nums.len() % 3 {
+            1 => {
+                three_digits -= 1;
+                2
+            },
+            2 => 1,
+            _ => 0,
+        };
+
+        for _ in 0..three_digits {
+            let mut part: String = "".to_string();
+            for _ in 0..3 {
+                part.push_str(&(nums.pop_front().unwrap().to_string()));
+            }
+            parts.push(part);
+        }
+
+        for _ in 0..two_digits {
+            let mut part: String = "".to_string();
+            for _ in 0..2 {
+                part.push_str(&(nums.pop_front().unwrap().to_string()));
+            }
+            parts.push(part);
+        }
+
+        parts.join("-"
+    }
+})
