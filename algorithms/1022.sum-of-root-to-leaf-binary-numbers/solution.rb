f16aee68358a8174cc368fa0d9ccbdd67ed86093
Kohei Sugimoto <18735471+kohbis@users.noreply.github.com>
2021-01-31 20:42:08 +0900

    solve 11022 in ruby

---
+++ algorithms/1022.sum-of-root-to-leaf-binary-numbers/solution.rb
@@ -0,0 +1,31 @@
+# Definition for a binary tree node.
+# class TreeNode
+#     attr_accessor :val, :left, :right
+#     def initialize(val = 0, left = nil, right = nil)
+#         @val = val
+#         @left = left
+#         @right = right
+#     end
+# end
+# @param {TreeNode} root
+# @return {Integer}
+def sum_root_to_leaf(root)
+  @sum = 0
+  dfs(root, '') if root
+  @sum
+end
+
+def dfs(root, current)
+  return unless root
+
+  current += root.val.to_s
+
+  if root.left.nil? && root.right.nil?
+    @sum += current.to_i(2)
+  end
+
+  dfs(root.left, current)
+  dfs(root.right, current)
+
+  current.chop
+end
