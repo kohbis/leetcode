5f19c9cc36b1f43a39ec40465715b3fc8a32485a
Kohei Sugimoto <18735471+kohbis@users.noreply.github.com>
2021-03-05 23:29:57 +0900

    solve 1763 in ruby

---
+++ algorithms/1763.longest-nice-substring/solution.rb
@@ -0,0 +1,24 @@
+# @param {String} s
+# @return {String}
+def longest_nice_substring(s)
+  return '' if s.length < 2
+
+  uniqs = s.chars.sort.uniq
+  # or
+  # uniqs = Set[*s.chars]
+
+  s.each_char.with_index do |c, i|
+    next if uniqs.include?(c.upcase) && uniqs.include?(c.downcase)
+
+    left = longest_nice_substring(s[0...i])
+    right = longest_nice_substring(s[i+1..])
+
+    if left.length >= right.length
+      return left
+    else
+      return right
+    end
+  end
+
+  s
+end
