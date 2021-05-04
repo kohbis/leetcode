69f14254e0016d3e8d69c4c0b499d5259043ffff
Kohei Sugimoto <18735471+kohbis@users.noreply.github.com>
2021-05-04 17:12:15 +0900

    update 1844

--- algorithms/1844.replace-all-digits-with-characters/solution.rb
+++ algorithms/1844.replace-all-digits-with-characters/solution.rb
@@ -1,8 +1,12 @@
 # @param {String} s
 # @return {String}
 def replace_digits(s)
-  alph = ('a'..'z').to_a
-  s.chars.each_slice(2).map {|c, n|
-    n ? [c, alph[alph.find_index(c) + n.to_i]] : c
-  }.join
+  s.chars.each_slice(2).flat_map {|c, n|
+    n ? [c.ord, c.ord + n.ord - '0'.ord] : c.ord
+  }.pack('c*')
+
+  # alph = ('a'..'z').to_a
+  # s.chars.each_slice(2).map {|c, n|
+    # n ? [c, alph[alph.find_index(c) + n.to_i]] : c
+  # }.join
 end
