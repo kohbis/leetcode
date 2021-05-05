ece93503c58bd3e6bbf9d1e391a3653cee95c39b
Kohei Sugimoto <18735471+kohbis@users.noreply.github.com>
2021-05-05 10:51:10 +0900

    update 1844

--- algorithms/1844.replace-all-digits-with-characters/solution.rs
+++ algorithms/1844.replace-all-digits-with-characters/solution.rs
@@ -3,12 +3,11 @@ impl Solution {
         let mut res: Vec<u8> = vec![];
         let mut pre = b'a';
         for (i, &v) in s.as_bytes().iter().enumerate() {
-            res.push(match i % 2 == 0 {
-                true => {
-                    pre = v;
-                    v
-                }
-                false => pre + v - b'0',
+            res.push(if i % 2 == 0 {
+                pre = v;
+                v
+            } else {
+                pre + v - b'0'
             })
         }
         String::from_utf8(res).unwrap()
