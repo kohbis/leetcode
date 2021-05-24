2085567cdc130c7f1a8c6471efd8526188ab88dc
Kohei Sugimoto <18735471+kohbis@users.noreply.github.com>
2021-05-24 09:51:58 +0900

    remove extra line

--- algorithms/1071.greatest-common-divisor-of-strings/solution.rs
+++ algorithms/1071.greatest-common-divisor-of-strings/solution.rs
@@ -5,7 +5,6 @@ impl Solution {
         } else if str2.len() > str1.len() {
             return Solution::gcd_of_strings(str2, str1);
         } else if str1.starts_with(&str2) {
-            println!("{}", str1[..str2.len()].to_string());
             return Solution::gcd_of_strings(str1[str2.len()..].to_string(), str2);
         }
 
