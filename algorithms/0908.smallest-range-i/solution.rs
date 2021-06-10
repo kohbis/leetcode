fdb7859d96f3581ffea85fcf02fb9349e9862844
Kohei Sugimoto <18735471+kohbis@users.noreply.github.com>
2021-06-10 20:09:09 +0900

    solve 908 in rust

---
+++ algorithms/0908.smallest-range-i/solution.rs
@@ -0,0 +1,6 @@
+impl Solution {
+    pub fn smallest_range_i(nums: Vec<i32>, k: i32) -> i32 {
+        // (max - k) - (min - k)
+        std::cmp::max(0, nums.iter().max().unwrap() - nums.iter().min().unwrap() - 2 * k)
+    }
+}
