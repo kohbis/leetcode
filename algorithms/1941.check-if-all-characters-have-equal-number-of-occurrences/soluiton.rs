33fe67d6d80e6148c9eaddeec8e32717ef744c0f
Kohei Sugimoto <18735471+kohbis@users.noreply.github.com>
2021-07-26 21:05:31 +0900

    solve 1941 in rust

---
+++ algorithms/1941.check-if-all-characters-have-equal-number-of-occurrences/soluiton.rs
@@ -0,0 +1,12 @@
+use std::collections::{HashMap, HashSet};
+
+impl Solution {
+    pub fn are_occurrences_equal(s: String) -> bool {
+        let mut counts: HashMap<char, i32> = HashMap::new();
+        for c in s.chars() {
+            *counts.entry(c).or_insert(0) += 1;
+        }
+
+        counts.into_iter().map(|(_, n)| n).collect::<HashSet<i32>>().len() == 1
+    }
+}
