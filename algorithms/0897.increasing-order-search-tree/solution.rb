232e7f99fc60ea96b9699c33bf6cfede08be6df5
Kohei Sugimoto <18735471+kohbis@users.noreply.github.com>
2021-08-24 23:36:11 +0900

    solve 897 in ruby

---
+++ algorithms/0897.increasing-order-search-tree/solution.rb
@@ -0,0 +1,26 @@
+# Definition for a binary tree node.
+# class TreeNode
+#     attr_accessor :val, :left, :right
+#     def initialize(val = 0, left = nil, right = nil)
+#         @val = val
+#         @left = left
+#         @right = right
+#     end
+# end
+# @param {TreeNode} root
+# @return {TreeNode}
+def increasing_bst(root)
+  nodes = build_node_array(root)
+
+  for i in 0...nodes.size
+    nodes[i].left = nil
+    nodes[i].right = nodes[i+1]
+  end
+
+  nodes[0]
+end
+
+def build_node_array(root)
+  return [] unless root
+  build_node_array(root.left) + [root] + build_node_array(root.right)
+end
