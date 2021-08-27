a9ca00bfa848751aadca2eda0fe2df2bc0e43015
Kohei Sugimoto <18735471+kohbis@users.noreply.github.com>
2021-08-27 23:57:02 +0900

    solve 1523 in ruby

---
+++ algorithms/1523,count-odd-numbers-in-an-interval-range/solution.rb
@@ -0,0 +1,15 @@
+# @param {Integer} low
+# @param {Integer} high
+# @return {Integer}
+def count_odds(low, high)
+  diff = high - low
+  sum = diff / 2
+
+  if diff % 2 == 1
+    sum += 1
+  elsif low % 2 == 1
+    sum += 1
+  end
+
+  sum   
+end
