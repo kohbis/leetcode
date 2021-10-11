701a10eae5c0351f2e38662a5e259e20fdbd65f2
Kohei Sugimoto <18735471+kohbis@users.noreply.github.com>
2021-10-11 23:55:41 +0900

    solve 345 in ruby

---
+++ algorithms/0345.reverse-vowels-of-a-string/solution.rb
@@ -0,0 +1,17 @@
+# @param {String} s
+# @return {String}
+def reverse_vowels(s)
+  vowels = ["a", "e", "i", "o", "u", "A", "E", "I", "O", "U"]
+  stack = s.chars.select { |x| vowels.include?(x) }
+
+  res = []
+  s.chars.each do |c|
+    if vowels.include?(c)
+      res << stack.pop
+    else
+      res << c
+    end
+  end
+
+  res.join
+end
