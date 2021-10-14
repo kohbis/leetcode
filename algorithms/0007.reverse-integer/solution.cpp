8e8fdf3aa36ef7d692f80119ee230c49000ac471
Kohei Sugimoto <18735471+kohbis@users.noreply.github.com>
2021-10-15 00:27:06 +0900

    solve 7 in cpp

---
+++ algorithms/0007.reverse-integer/solution.cpp
@@ -0,0 +1,17 @@
+class Solution {
+  public:
+    int reverse(int x) {
+        long long res = 0;
+
+        while (x != 0) {
+            res = res * 10 + x % 10;
+            x /= 10;
+        }
+
+        if (res > INT_MAX || INT_MIN > res) {
+            return 0;
+        } else {
+            return res;
+        }
+    }
+};
