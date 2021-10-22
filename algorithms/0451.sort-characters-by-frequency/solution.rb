289154cb25917d01f3fdf0a6120578b1295e3448
Kohei Sugimoto <18735471+kohbis@users.noreply.github.com>
2021-10-23 00:21:37 +0900

    solve 451 in ruby

---
+++ algorithms/0451.sort-characters-by-frequency/solution.rb
@@ -0,0 +1,7 @@
+# @param {String} s
+# @return {String}
+def frequency_sort(s)
+  counter = Hash.new(0)
+  s.each_char { |c| counter[c] += 1 }
+  counter.sort_by { |_, v| -v }.inject("") { |res, item| res + item[0] * item[1] }
+end
