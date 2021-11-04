70adb2c8c3c5865ebb89b3f2d9a619fc4309e976
Kohei Sugimoto <18735471+kohbis@users.noreply.github.com>
2021-11-04 23:53:33 +0900

    solve 2057 in cpp,go

---
+++ algorithms/2057.smallest-index-with-equal-value/solution.cpp
@@ -0,0 +1,18 @@
+#include <vector>
+
+using namespace std;
+
+class Solution {
+  public:
+    int smallestEqual(vector<int>& nums) {
+        int n = nums.size();
+
+        for (int i = 0; i < n; i++) {
+            if (i % 10 == nums[i]) {
+                return i;
+            }
+        }
+
+        return -1;
+    }
+};
