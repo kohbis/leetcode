358833db7227ef21ce021c05923a3b8345fa20fc
Kohei Sugimoto <18735471+kohbis@users.noreply.github.com>
2021-11-27 08:35:12 +0900

    solve 35 in rust,cpp

--- algorithms/0035.search-insert-position/solution.rs
+++ algorithms/0035.search-insert-position/solution.rs
@@ -1,19 +1,23 @@
 impl Solution {
     pub fn search_insert(nums: Vec<i32>, target: i32) -> i32 {
-        let mut res: i32 = 0;
-        let &max = nums.last().unwrap();
+        if target < nums[0] {
+            return 0i32;
+        }
 
-        if target > max {
-            res = nums.len() as i32;
-        } else {
-            for (i, &x) in nums.iter().enumerate() {
-                if x >= target {
-                    res = i as i32;
-                    break;
-                }
+        let (mut left, mut right) = (0, nums.len() - 1);
+
+        while left <= right {
+            let mid = left + (right - left) / 2;
+
+            if (nums[mid] == target) {
+                return mid as _;
+            } else if (nums[mid] > target) {
+                right = mid - 1;
+            } else {
+                left = mid + 1;
             }
         }
 
-        res
+        left as _
     }
 }
