5e26216f91395939ec7837b9aa53f174ab30fc68
Kohei Sugimoto <18735471+kohbis@users.noreply.github.com>
2021-12-03 08:59:46 +0900

    solve 2078 in cpp

---
+++ algorithms/2078.two-furthest-houses-with-different-colors/solution.cpp
@@ -0,0 +1,22 @@
+#include <vector>
+
+using namespace std;
+
+class Solution {
+  public:
+    int maxDistance(vector<int>& colors) {
+        int len = colors.size();
+
+        int left = 0;
+        while (colors[left] == colors[len - 1]) {
+            left++;
+        }
+
+        int right = len - 1;
+        while (colors[0] == colors[right]) {
+            right--;
+        }
+
+        return max((len - 1 - left), right);
+    }
+};
