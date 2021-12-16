6c8a896bc507f754f6dd65d5db103da8d7e7aeae
Kohei Sugimoto <18735471+kohbis@users.noreply.github.com>
2021-12-16 14:24:52 +0900

    solve 2006 in cpp

---
+++ algorithms/2006.count-number-of-pairs-with-absolute-difference-k/solution.cpp
@@ -0,0 +1,21 @@
+#include <vector>
+
+using namespace std;
+
+class Solution {
+  public:
+    int countKDifference(vector<int>& nums, int k) {
+        int count = 0;
+        size_t size = nums.size();
+
+        for (int i = 0; i < (size - 1); i++) {
+            for (int j = (i + 1); j < size; j++) {
+                if (abs(nums[i] - nums[j]) == k) {
+                    count++;
+                }
+            }
+        }
+
+        return count;
+    }
+};
