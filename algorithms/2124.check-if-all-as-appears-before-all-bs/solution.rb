72f284050a003445df8c416c9d8c11b50e51d044
Kohei Sugimoto <18735471+kohbis@users.noreply.github.com>
2022-01-10 00:24:20 +0900

    solve 2124 in ruby

---
+++ algorithms/2124.check-if-all-as-appears-before-all-bs/solution.rb
@@ -0,0 +1,16 @@
+# @param {String} s
+# @return {Boolean}
+def check_string(s)
+  appeared = false
+
+  s.chars.each do |c|
+    case c
+    when "a"
+      return false if appeared
+    when "b"
+      appeared = true
+    end
+  end
+
+  true
+end
