fd8a909940448a62201fe6024b74743043eea2c0
Kohei Sugimoto <18735471+kohbis@users.noreply.github.com>
2022-01-17 01:08:46 +0900

    solve 2129 in ruby

---
+++ algorithms/2129.capitalize-the-title/solution.rb
@@ -0,0 +1,16 @@
+# @param {String} title
+# @return {String}
+def capitalize_title(title)
+  words = []
+
+  title.split.each do |word|
+    tmp = word.downcase
+    if word.size > 2
+      words << tmp[0].upcase + tmp[1..]
+    else
+      words << tmp
+    end
+  end
+
+  words.join(" ")
+end
