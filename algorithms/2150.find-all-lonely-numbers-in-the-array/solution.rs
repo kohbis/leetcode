c1102d2407487dc65351472bf894430b26eac83e
Kohei Sugimoto <18735471+kohbis@users.noreply.github.com>
2022-02-05 16:44:20 +0900

    solve 2150 in rust

---
+++ algorithms/2150.find-all-lonely-numbers-in-the-array/solution.rs
@@ -0,0 +1,25 @@
+use std::collections::HashMap;
+
+impl Solution {
+    pub fn find_lonely(nums: Vec<i32>) -> Vec<i32> {
+        let mut hash: HashMap<i32, i32> = HashMap::new();
+
+        for &num in &nums {
+            let entry = hash.entry(num).or_default();
+            *entry += 1;
+        }
+
+        let mut ans: Vec<i32> = vec![];
+        for num in nums {
+            if let Some(value) = hash.get(&num) {
+                if *value == 1 {
+                    if !hash.contains_key(&(num + 1)) && !hash.contains_key(&(num - 1)) {
+                        ans.push(num);
+                    }
+                }
+            }
+        }
+
+        ans
+    }
+}
