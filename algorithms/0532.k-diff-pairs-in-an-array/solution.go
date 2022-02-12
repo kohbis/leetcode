ff1cd05903c2dfe98373f2c7e24fb95be00d0ae0
Kohei Sugimoto <18735471+kohbis@users.noreply.github.com>
2022-02-12 10:25:26 +0900

    solve 532 in go

---
+++ algorithms/0532.k-diff-pairs-in-an-array/solution.go
@@ -0,0 +1,21 @@
+func findPairs(nums []int, k int) int {
+	m := map[int]int{}
+	for _, n := range nums {
+		if _, ok := m[n]; ok {
+			m[n] += 1
+		} else {
+			m[n] = 1
+		}
+	}
+
+	ans := 0
+	for key, value := range m {
+		if _, ok := m[key+k]; ok {
+			if (k == 0 && value > 1) || k > 0 {
+				ans++
+			}
+		}
+	}
+
+	return ans
+}
