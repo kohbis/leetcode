37e9c6e6c7b46871a727510700789ee857896547
Kohei Sugimoto <18735471+kohbis@users.noreply.github.com>
2022-02-15 19:40:04 +0900

    solve 104 in rust

---
+++ algorithms/0104.maximum-depth-of-binary-tree/solution.rs
@@ -0,0 +1,32 @@
+// Definition for a binary tree node.
+// #[derive(Debug, PartialEq, Eq)]
+// pub struct TreeNode {
+//   pub val: i32,
+//   pub left: Option<Rc<RefCell<TreeNode>>>,
+//   pub right: Option<Rc<RefCell<TreeNode>>>,
+// }
+//
+// impl TreeNode {
+//   #[inline]
+//   pub fn new(val: i32) -> Self {
+//     TreeNode {
+//       val,
+//       left: None,
+//       right: None
+//     }
+//   }
+// }
+use std::cell::RefCell;
+use std::rc::Rc;
+impl Solution {
+    pub fn max_depth(root: Option<Rc<RefCell<TreeNode>>>) -> i32 {
+        match root {
+            Some(node) => {
+                let left = Solution::max_depth(node.borrow().left.clone());
+                let right = Solution::max_depth(node.borrow().right.clone());
+                return 1 + std::cmp::max(left, right);
+            }
+            None => return 0,
+        }
+    }
+}
