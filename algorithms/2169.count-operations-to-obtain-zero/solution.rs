d588eff57921375f33324f5f4afadfbeb52328b1
Kohei Sugimoto <18735471+kohbis@users.noreply.github.com>
2022-02-20 11:50:05 +0900

    solve 2169 in rust

---
+++ algorithms/2169.count-operations-to-obtain-zero/solution.rs
@@ -0,0 +1,18 @@
+impl Solution {
+    pub fn count_operations(num1: i32, num2: i32) -> i32 {
+        let (mut num1, mut num2) = (num1, num2);
+        let mut ans = 0i32;
+
+        while num1 > 0 && num2 > 0 {
+            if num1 < num2 {
+                num2 -= num1;
+            } else {
+                num1 -= num2;
+            }
+
+            ans += 1;
+        }
+
+        ans
+    }
+}
