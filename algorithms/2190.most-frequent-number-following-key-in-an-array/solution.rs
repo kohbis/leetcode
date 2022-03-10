a3f282eb5004aed01f92a491f3f356ce241c445e
Kohei Sugimoto <18735471+kohbis@users.noreply.github.com>
2022-03-11 00:28:11 +0900

    solve 2190 in rust

---
+++ algorithms/2190.most-frequent-number-following-key-in-an-array/solution.rs
@@ -0,0 +1,23 @@
+use std::collections::HashMap;
+
+impl Solution {
+    pub fn most_frequent(nums: Vec<i32>, key: i32) -> i32 {
+        let mut map: HashMap<i32, i32> = HashMap::new();
+
+        for i in 0..nums.len() - 1 {
+            if nums[i] == key {
+                *map.entry(nums[i + 1]).or_default() += 1;
+            }
+        }
+
+        let (mut ans, mut curr) = (0, 0);
+        for (k, v) in map {
+            if curr < v {
+                curr = v;
+                ans = k;
+            }
+        }
+
+        ans
+    }
+}
