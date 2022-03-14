26e9c51876640bb220ccc332b85e1e56d7a2b1ec
Kohei Sugimoto <18735471+kohbis@users.noreply.github.com>
2022-03-14 11:49:40 +0900

    solve 0071 in ruby

---
+++ algorithms/0071.simplify-path/solution.rb
@@ -0,0 +1,18 @@
+# @param {String} path
+# @return {String}
+def simplify_path(path)
+  stack = []
+
+  path.split("/") do |dir|
+    case dir
+    when "", "."
+      # do nothing
+    when ".."
+      stack.pop if stack.any?
+    else
+      stack.push(dir)
+    end
+  end
+
+  "/" + stack.join("/")
+end
