f849d1871c9d73f578d91907e7a2ebf8fce5d402
Kohei Sugimoto <18735471+kohbis@users.noreply.github.com>
2022-04-10 20:02:28 +0900

    algorithm 682 in ruby

---
+++ algorithms/0682.baseball-game/solution.rb
@@ -0,0 +1,20 @@
+# @param {String[]} ops
+# @return {Integer}
+def cal_points(ops)
+  scores = []
+
+  ops.each do |op|
+    case op
+    when "C"
+      scores.pop
+    when "D"
+      scores.push(scores.last * 2)
+    when "+"
+      scores.push(scores.last(2).sum)
+    else
+      scores.push(op.to_i)
+    end
+  end
+
+  scores.sum
+end
