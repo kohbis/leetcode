6f915f879dd8c9ac0a92b635ce14b87a6742348d
Kohei Sugimoto <18735471+kohbis@users.noreply.github.com>
2022-04-12 23:39:59 +0900

    algorithm 2236 in rust

---
+++ algorithms/2236.root-equals-sum-of-children/solution.rs
@@ -0,0 +1,36 @@
+// Definition for a binary tree node.
+// #[derive(Debug, PartialEq, Eq)]
+// pub struct TreeNode {
+//   pub val: i32,
+//   pub left: Option<Rc<RefCell<TreeNode>>>,
+//   pub right: Option<Rc<RefCell<TreeNode>>>,
+// }
+//
+// impl TreeNode {
+//   #[inline]
+//   pub fn new(val: i32) -> Self {
+//     TreeNode {
+//       val,
+//       left: None,
+//       right: None
+//     }
+//   }
+// }
+use std::cell::RefCell;
+use std::rc::Rc;
+impl Solution {
+    pub fn check_tree(root: Option<Rc<RefCell<TreeNode>>>) -> bool {
+        match root {
+            Some(node) => {
+                let node = node.borrow();
+                match (node.left.as_ref(), node.right.as_ref()) {
+                    (Some(left), Some(right)) => {
+                        return left.borrow().val + right.borrow().val == node.val
+                    }
+                    _ => true,
+                }
+            }
+            None => true,
+        }
+    }
+}
