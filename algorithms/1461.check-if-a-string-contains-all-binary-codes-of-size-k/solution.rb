726097954143055271b15d300d776cf18240981e
Kohei Sugimoto <18735471+kohbis@users.noreply.github.com>
2022-06-01 08:59:31 +0900

    algorithms 1461 in ruby

---
+++ algorithms/1461.check-if-a-string-contains-all-binary-codes-of-size-k/solution.rb
@@ -0,0 +1,13 @@
+# @param {String} s
+# @param {Integer} k
+# @return {Boolean}
+def has_all_codes(s, k)
+  return false if s.size < k
+
+  set = Set.new
+  (0..(s.size - k)).each do |i|
+    set << s[i...i + k]
+  end
+
+  set.size == 2**k
+end
