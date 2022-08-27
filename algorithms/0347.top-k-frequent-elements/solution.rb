c3742dd0073541b147697eb952711f4ecfcb3502
kohbis <18735471+kohbis@users.noreply.github.com>
2022-08-28 01:15:49 +0900

    rufo

--- algorithms/0347.top-k-frequent-elements/solution.rb
+++ algorithms/0347.top-k-frequent-elements/solution.rb
@@ -2,5 +2,5 @@
 # @param {Integer} k
 # @return {Integer[]}
 def top_k_frequent(nums, k)
-   nums.tally.sort_by {|_, v| -v }.map {|k, _| k }[...k]
+  nums.tally.sort_by { |_, v| -v }.map { |k, _| k }[...k]
 end
--- algorithms/0929.unique-email-addresses/solution.rb
+++ algorithms/0929.unique-email-addresses/solution.rb
@@ -1,7 +1,8 @@
 # @param {String[]} emails
 # @return {Integer}
 def num_unique_emails(emails)
-  emails.map { |email|plus, at = email.index('+'), email.index('@')
+  emails.map { |email|
+    plus, at = email.index('+'), email.index('@')
     local = (plus.nil? ? email[...at] : email[...plus]).gsub('.', '')
     local << email[email.index('@')..]
   }.uniq.size
