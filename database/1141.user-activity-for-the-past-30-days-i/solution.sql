98c54608a4ea2e2191b157acb36e6b893853c7c6
kohbis <18735471+kohbis@users.noreply.github.com>
2022-08-29 22:23:58 +0900

    database 1141

---
+++ database/1141.user-activity-for-the-past-30-days-i/solution.sql
@@ -0,0 +1,6 @@
+SELECT activity_date AS DAY,
+       count(DISTINCT user_id) AS active_users
+FROM activity
+WHERE datediff('2019-07-27', activity_date) < 30
+  AND activity_date <= '2019-07-27'
+GROUP BY activity_date
