26a9a82060f66165d6c0fdd9e9641be65f7a971f
kohbis <18735471+kohbis@users.noreply.github.com>
2022-10-01 00:15:20 +0900

    database 1084

---
+++ database/1084.sales-analysis-iii/solution.sql
@@ -0,0 +1,7 @@
+SELECT s.product_id,
+       p.product_name
+FROM Sales s
+INNER JOIN Product p ON s.product_id = p.product_id
+GROUP BY s.product_id
+HAVING MIN(s.sale_date) >= '2019-01-01'
+AND MAX(s.sale_date) <= '2019-03-31'
