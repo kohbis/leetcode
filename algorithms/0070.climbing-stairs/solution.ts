c69e68a0e0db0c1ead7e1a0f93be9821760eb258
kohbis <18735471+kohbis@users.noreply.github.com>
2022-12-12 20:29:33 +0900

    prettier

--- algorithms/0070.climbing-stairs/solution.ts
+++ algorithms/0070.climbing-stairs/solution.ts
@@ -1,16 +1,16 @@
 function climbStairs(n: number): number {
-    const a = new Array<number>(n).fill(0);
-    for (let i = 0; i < n; i++) {
-        switch (i) {
-            case 0:
-                a[i] = 1;
-                break;
-            case 1:
-                a[i] = 2;
-                break;
-            default:
-                a[i] = a[i - 2] + a[i - 1];
-        }
+  const a = new Array<number>(n).fill(0);
+  for (let i = 0; i < n; i++) {
+    switch (i) {
+      case 0:
+        a[i] = 1;
+        break;
+      case 1:
+        a[i] = 2;
+        break;
+      default:
+        a[i] = a[i - 2] + a[i - 1];
     }
-    return a[n - 1];
-};
+  }
+  return a[n - 1];
+}
