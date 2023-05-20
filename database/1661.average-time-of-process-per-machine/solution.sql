68b462272f0ebe9e9252456e41f4ba770b893f67
kohbis <18735471+kohbis@users.noreply.github.com>
2023-05-20 20:26:26 +0900

    databaase 1661

---
+++ database/1661.average-time-of-process-per-machine/solution.sql
@@ -0,0 +1,14 @@
+select
+  machine_id,
+  round(
+    sum(
+      case
+        when activity_type = "start" then -timestamp
+        else timestamp
+      end
+    ) / count(distinct process_id), 3
+  ) as processing_time
+from
+  activity
+group by
+  machine_id
