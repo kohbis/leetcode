30ddc874cfb602d60bd68f0d84c1cff6b7e50461
kohbis <18735471+kohbis@users.noreply.github.com>
2024-01-05 23:16:04 +0900

    algorithms 300 in cpp

---
+++ algorithms/0300.longest-increasing-subsequence/solution.cpp
@@ -0,0 +1,27 @@
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    int lengthOfLIS(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> dp(n, 1);
+
+        int longest = 1;
+        for (int i = 0; i < n; ++i) {
+            for (int j = 0; j < i; ++j) {
+                if (nums[j] < nums[i]) {
+                    if (dp[i] < dp[j] + 1) {
+                        dp[i] = dp[j] + 1;
+                    }
+                    if (longest < dp[i]) {
+                        longest = dp[i];
+                    }
+                }
+            }
+        }
+
+        return longest;
+    }
+};
