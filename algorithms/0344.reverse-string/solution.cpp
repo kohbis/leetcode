4724ce175215eda134498c23db255290e4096f25
kohbis <18735471+kohbis@users.noreply.github.com>
2024-06-02 11:57:11 +0900

    algorithms 344 in cpp

---
+++ algorithms/0344.reverse-string/solution.cpp
@@ -0,0 +1,24 @@
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    void reverseString(vector<char>& s) {
+        if (s.size() == 0) {
+            return;
+        }
+
+        int i = 0;
+        int j = s.size() - 1;
+        while (i < j) {
+            char temp = s[i];
+            s[i] = s[j];
+            s[j] = temp;
+            i++;
+            j--;
+        }
+
+        return;
+    }
+};
