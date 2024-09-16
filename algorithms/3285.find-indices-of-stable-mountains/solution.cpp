cd87239019fb42171ff10b3db341412d72339eed
kohbis <18735471+kohbis@users.noreply.github.com>
2024-09-16 09:02:05 +0900

    algorithms 3285 in cpp

---
+++ algorithms/3285.find-indices-of-stable-mountains/solution.cpp
@@ -0,0 +1,16 @@
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    vector<int> stableMountains(vector<int>& height, int threshold) {
+        vector<int> res;
+        for (int i = 1; i < height.size(); i++) {
+            if (height[i - 1] > threshold) {
+                res.push_back(i);
+            }
+        }
+        return res;
+    }
+};
